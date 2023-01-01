851976a67cc24b9629abdbe516cebc5b948b4457
Ugaso Sheikh-Abdi <ugotsoul@users.noreply.github.com>
2022-12-31 22:38:22 -0800

    first commit; abliity to harvest and transform existing coffee beans into basic quality

---
+++ BasicCoffee/ModConfig.cs
@@ -0,0 +1,9 @@
+﻿using StardewModdingAPI.Utilities;
+
+namespace BasicCoffee
+{
+    internal class ModConfig
+    {
+        public KeybindList ToggleKey { get; set; } = KeybindList.Parse("C + RightWindows");
+    }
+}
---
+++ BasicCoffee/ModEntry.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace BasicCoffee
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Properties
+        *********/
+        /// <summary>The mod configuration from the player.</summary>
+        private ModConfig Config;
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+
+            this.Config = this.Helper.ReadConfig<ModConfig>();
+            helper.Events.Player.InventoryChanged += this.CheckItem;
+            helper.Events.Input.ButtonsChanged += this.OnButtonsChanged;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+
+        private void CheckItem(object sender, InventoryChangedEventArgs e)
+        {
+            foreach (Item item in e.Added)
+            {
+                if (item.ParentSheetIndex == 433)
+                {
+                    this.Monitor.Log($"Picked up {item.DisplayName} item with quality of {(item as StardewValley.Object).quality}", LogLevel.Debug);
+
+                    if ((item as StardewValley.Object).Quality > 0) {
+                        (item as StardewValley.Object).Quality = 0;
+                    }
+
+                }
+            }
+        }
+
+        private void OnButtonsChanged(object sender, ButtonsChangedEventArgs e)
+        {
+            if (this.Config.ToggleKey.JustPressed())
+            {
+                // remove quality from all coffee beans in your items
+                var coffeeBeansChanged = 0;
+
+                Utility.iterateAllItems(item =>
+                {
+                    if (item.ParentSheetIndex == 433 && (item as StardewValley.Object).Quality > 0)
+                    {
+                        (item as StardewValley.Object).Quality = 0;
+                        coffeeBeansChanged++;
+                    }
+                });
+
+                if (coffeeBeansChanged > 0) 
+                    this.Monitor.Log($"Changed quality of {coffeeBeansChanged} coffee beans into basic beans", LogLevel.Debug);
+            }
+        }
+
+    }
+}
